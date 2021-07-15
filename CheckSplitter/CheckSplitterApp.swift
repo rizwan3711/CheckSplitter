ae8e82ece270e48870140bfcabf467b1f6177846
rizwan3711 <84203059+rizwan3711@users.noreply.github.com>
2021-07-14 23:20:31 -0700

    Initial Commit

---
+++ CheckSplitter/CheckSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitterApp.swift
+//  CheckSplitter
+//
+//  Created by Rizwan Syed on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
